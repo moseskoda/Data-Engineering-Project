05441b068c991f4992f7b12c337ca1f4f7bf90bf
Moses Moe <mkoda16@yahoo.com>
2026-06-19 23:17:17 +0000

    #another comment from dbt cloud

--- models/Untitled-1.sql
+++ models/Untitled-1.sql
@@ -4,6 +4,7 @@ select * from dbt-tutorial.stripe.payment
 #another comment from dbt cloud
 #another comment from dbt cloud
 #another comment from dbt cloud
+#another comment from dbt cloud
 =======
 #this is a comment line
 select * from dbt-tutorial.stripe.payment
