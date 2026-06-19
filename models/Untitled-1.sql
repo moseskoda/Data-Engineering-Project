cdbb33b1546c3936901a949c3fe5155f64412b59
moseskoda <mkoda16@yahoo.com>
2026-06-19 19:14:59 -0400

    Update Untitled-1.sql

--- models/Untitled-1.sql
+++ models/Untitled-1.sql
@@ -3,6 +3,7 @@ select * from dbt-tutorial.stripe.payment
 #this is comment from dtb cloud
 #another comment from dbt cloud
 #another comment from dbt cloud
+#another comment from dbt cloud
 =======
 #this is a comment line
 select * from dbt-tutorial.stripe.payment
